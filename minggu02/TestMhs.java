041188333bbfd1592223034ecdc5995e6c410573
Diah Putri Nofianti <90998386+Diaputr@users.noreply.github.com>
2022-09-11 21:43:07 +0700

    Add files via upload

---
+++ minggu02/TestMhs.java
@@ -0,0 +1,14 @@
+package minggu02;
+public class TestMhs {
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.nim = 101;
+        mhs1.nama = "Lestari";
+        mhs1.alamat = "Jl. Vinola no. 1A";
+        mhs1.kelas = "1A";
+        mhs1.tampilBiodata();
+        
+        Mahasiswa mhs2 = new Mahasiswa();
+        Mahasiswa mhs3 = new Mahasiswa();
+    }
+}
